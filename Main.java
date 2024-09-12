8e66e0ff88819b68d77340259670b9a08b024fcb
Christian Pérez <92156352+Chriistiianx@users.noreply.github.com>
2024-09-12 08:39:07 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(conversorTemperatura.celsiusaFahrenheit( 41));
-        System.out.println(conversorTemperatura.fahrenheitaCelsius( 100));
-    }
-}
